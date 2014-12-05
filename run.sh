13583d9dbb46d842fcee5038853bc1d8f2b35e8d
Gauthier C <gc@ac-lille.fr>
2014-12-05 15:55:10 +0100

    Hide xorg error

--- run.sh
+++ run.sh
@@ -17,5 +17,5 @@ sed -i "s/XKBLAYOUT=.*/XKBLAYOUT=\"$SPICE_KB\"/" /etc/default/keyboard
 sed -i "s/SPICE_KB/$SPICE_KB/" /etc/xdg/autostart/keyboard.desktop
 sed -i "s/SPICE_RES/$SPICE_RES/" /etc/xdg/autostart/resolution.desktop
 cd /home/$SPICE_USER
-su $SPICE_USER -c "/usr/bin/Xorg -config /etc/X11/spiceqxl.xorg.conf -logfile  /home/$SPICE_USER/.Xorg.2.log :2 &"
+su $SPICE_USER -c "/usr/bin/Xorg -config /etc/X11/spiceqxl.xorg.conf -logfile  /home/$SPICE_USER/.Xorg.2.log :2 &" 2> /dev/null
 su $SPICE_USER -c "DISPLAY=:2 /usr/bin/xfce4-session"
